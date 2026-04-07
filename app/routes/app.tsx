5e30e3fa02b10e08ce888f1b6a87d2c31866d7b8
Simone De Vivo <simonedevivo1996@gmail.com>
2026-04-07 10:17:08 +0000

    Update nav link to images page

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -24,7 +24,7 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/images">🖼️ Upscaling Immagini</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
